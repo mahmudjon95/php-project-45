86c3b20356f5119d7f87956f63d88d6f982cc0d4
Mahmudjon <mahmudjon.sayfiddinov@gmail.com>
2023-07-02 17:54:15 +0500

    clearte file engine for games

---
+++ src/Engine.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Engine File Doc Comment
+ * PHP version 8
+ * 
+ * @category Engine
+ * @package  MyPackage
+ * @author   Mahmudjon <mahmudjon.sayfiddinov@gmail.com> 
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     http://hexlet.io
+ */
