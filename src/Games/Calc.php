3607b3bed010f6989a1da116dd16812d05aff659
Mahmudjon <mahmudjon.sayfiddinov@gmail.com>
2023-07-03 13:19:02 +0500

    clearte file

---
+++ src/Games/Calc.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Calc File Doc Comment
+ * PHP version 8
+ * 
+ * @category Calc
+ * @package  MyPackage
+ * @author   Mahmudjon <mahmudjon.sayfiddinov@gmail.com> 
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     http://hexlet.io
+ */
+
+namespace Src\Games\Calc;
+
+/**
+ * Processes Even game
+ * 
+ * @return boolean
+ */
+function calcStart()
+{
+
+    
+}
