b841bbd421619175cd8e0e1ee8d6b6b66a2bdaa5
Mahmudjon <mahmudjon.sayfiddinov@gmail.com>
2023-07-12 22:51:12 +0500

    edit line Welcome to ... Game! to -> Games

--- src/Engine.php
+++ src/Engine.php
@@ -27,7 +27,7 @@ const GAMES_COUNT = 3;
  */
 function startGame($gameAbout, $gameDate)
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line($gameAbout);
