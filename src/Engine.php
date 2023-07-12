52a29396fd0b94b51decde053fde5fbd0273df3a
Mahmudjon <mahmudjon.sayfiddinov@gmail.com>
2023-07-12 18:45:45 +0500

    edit logic for Congratulations

--- src/Engine.php
+++ src/Engine.php
@@ -37,9 +37,8 @@ function startGame($gameAbout, $gameDate)
             line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
             line("Let's try again, {$name}!");
             return;
-        } else {
-            line("Correct!");
-        }
+        } 
+        line("Correct!");
     }
         line("Congratulations, {$name}");
 }
