19153daf53c93e10b154b7fc21b7c42ac9acd30a
Mahmudjon <mahmudjon.sayfiddinov@gmail.com>
2023-07-12 22:54:57 +0500

    edit line Welcome to ... Game! to -> Games

--- src/Cli.php
+++ src/Cli.php
@@ -23,7 +23,7 @@ use function cli\prompt;
  */
 function helloUser()
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 }
